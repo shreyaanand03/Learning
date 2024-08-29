2af18e306844dca0dda5f9fc7675eaab4fcced8f
shreyaanand03 <shreya.anand@divisionsinc.com>
2024-08-29 18:53:17 +0530

    pushing new repo

---
+++ promptTest.js
@@ -0,0 +1,8 @@
+const prompt=require("prompt-sync")({sigint:true});
+let num1=prompt("Enter first Number")
+console.log(num1);
+let num2=prompt("Enter second Number")
+console.log(num2)
+sum=parseInt(num1)+parseInt(num2)
+console.log(sum)
+
